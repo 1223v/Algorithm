03c311e13f0c51d602271f663616ed33d083cb8f
1223v <1223v@naver.com>
2024-03-12 00:55:38 +0900

    Feat: 11660

--- beakjoon/silver/11660.py
+++ beakjoon/silver/11660.py
@@ -1,28 +1,19 @@
 import sys
+input = sys.stdin.readline
 n, m = map(int,sys.stdin.readline().split())
 
-sumA = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-pointL = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+A = [[0] * (n+1)]
+D = [[0]* (n+1) for _ in range(n+1)]
 
-
-print(pointL)
-print(sumA)
-sumL = [[0 for _ in range(n)] for _ in range(n)]
-
-cnt = 0
 for i in range(n):
-    for j in range(n):
-        cnt += sumA[i][j]
-        sumL[i][j] = cnt
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
 
-print(sumL)
-# x2,y2 - x1,y1 로직
-for i in range(m):
-    # print(sumL[(pointL[i][3])-1][(pointL[i][2])-1])
-    # print(sumL[(pointL[i][1])-1][(pointL[i][0])-1])
-    if(sumL[(pointL[i][3])-1][(pointL[i][2])-1] == sumL[(pointL[i][1])-1][(pointL[i][0])-1]):
-        print(sumL[(pointL[i][3]) - 1][(pointL[i][2]) - 1])
-        print("메롤")
-    else:
-        print(sumL[(pointL[i][3]) - 1][(pointL[i][2]) - 1] - sumL[(pointL[i][1]) - 1][(pointL[i][0]) - 1])
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
 
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    print(result)
