911f2e67bf763a4f53da37854c58951bc8fc7bdd
1223v <1223v@naver.com>
2024-03-25 02:48:39 +0900

    Fix: 1929 연산 최적화

--- beakjoon/silver/1929.py
+++ beakjoon/silver/1929.py
@@ -4,13 +4,18 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-for i in range (n, m+1):
-    if i == 1:
-        continue
+
+def check(num):
+    if num == 1:
+        return False
     # int(i ** 1/2)제곱근까지만 나눠봐도 소수임을 판단 가능 36의 경우 1,2,3,4,5,6 까지 나눠보고 소수 판단 가능
-    for j in range(2, int(i ** 1/2) + 1):
+    for j in range(2, int(num ** 0.5) + 1): # 최대한 소수점을 쓸것
         if i % j == 0:
-            break
+            return False
 
     else:
+        return True
+
+for i in range (n, m+1):
+    if check(i):
         print(i)
