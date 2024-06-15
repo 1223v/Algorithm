2ed3dfece787604323bae48a3d5254f6889fea5b
Cho hyun-sik <78861124+1223v@users.noreply.github.com>
2024-06-16 02:37:24 +0900

    Feat: 17298

--- beakjoon/gold/17298.py
+++ beakjoon/gold/17298.py
@@ -1,5 +1,8 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-ans = [0] * n
+ans = [-1] * n # 0이 아닌 -1로 진행하면 마지막 주석된 while문 제거 가능 
 A = list(map(int, input().split()))
 myStack = []
 
@@ -8,11 +11,13 @@ for i in range(n):
         ans[myStack.pop()] = A[i] # 정답 리스트에 오큰수 저장
     myStack.append(i)
 
-while myStack:
-    ans[myStack.pop()] = -1
+# while myStack:
+#     ans[myStack.pop()] = -1
 
-result = ""
-for i in range(n):
-    result += str(ans[i])+" "
+# result = ""
+# for i in range(n):
+#     result += str(ans[i])+" "
 
-print(result)
+print(*ans)
+
+# print(result)
