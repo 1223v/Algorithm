ede14349041dfed4e1be8f7b51c078abd106f735
1223v <1223v@naver.com>
2024-06-18 16:28:24 +0900

    Feat: 2750_bubblesort

---
+++ beakjoon/bronze/2750_bubblesort.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+A = [0] * N
+
+for i in range(N):
+    A[i] = int(input())
+
+for i in range(N-1):
+    for j in range(N-1-i):
+        if A[j] > A[j+1]:
+            temp = A[j]
+            A[j] = A[j+1]
+            A[j+1] = temp
+
+for i in A:
+    print(i)
