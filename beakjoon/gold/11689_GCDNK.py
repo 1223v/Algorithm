7e9408c83a7a468a03208fccb7d74872dabf3f3e
1223v <1223v@naver.com>
2024-07-25 02:12:13 +0900

    Feat: 11689

---
+++ beakjoon/gold/11689_GCDNK.py
@@ -0,0 +1,17 @@
+import math
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+result = N
+
+for i in range(2, int(math.sqrt(N))+1):
+    if N % i == 0:
+        result -= result / i
+        while N % i == 0:
+            N /= i
+
+if N > 1:
+    result -= result /N
+
+print(int(result))
