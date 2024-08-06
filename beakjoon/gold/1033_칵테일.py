abfdeb1f0d8f0d9b748ed690f223d77f54366c08
1223v <1223v@naver.com>
2024-08-06 16:55:12 +0900

    Fix: 1033_칵테일

--- beakjoon/gold/1033_칵테일.py
+++ beakjoon/gold/1033_칵테일.py
@@ -1,15 +1,14 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
 
+N = int(input())
 A = [[] for _ in range(N)]
 visited = [False] * (N)
-
-D = [0] *(N)
+D = [0] * (N)
 lcm = 1
 
 def gcd(a,b):
-    if b==0:
+    if b == 0:
         return a
     else:
         return gcd(b, a%b)
@@ -19,21 +18,23 @@ def dfs(v):
     for i in A[v]:
         next = i[0]
         if not visited[next]:
-            D[next] = D[v] * i[2] // i[1]
+            D[next] = D[v] * i[2] // i[1] # 다음 노드와 지금 노드의 비율
             dfs(next)
+
 
 for i in range(N-1):
     a,b,p,q = map(int, input().split())
-    A[a].append((b,p,q))
+    A[a].append((b, p, q))
     A[b].append((a, q, p))
-    lcm *= (p * q // gcd(p,q))
+    lcm *= p * q // gcd(p,q)
 
 D[0] = lcm
-dfs(0)
 mgcd = D[0]
+dfs(0)
 
-for i in range(1,N):
+
+for i in range(N):
     mgcd = gcd(mgcd, D[i])
 
 for i in range(N):
-    print(int(D[i] // mgcd), end='')
+    print((D[i]//mgcd), end=' ')
