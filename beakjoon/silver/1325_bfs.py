d1af4d322cc396b123c7757d0b9c32f08d87cac1
1223v <1223v@naver.com>
2024-08-23 01:59:11 +0900

    Fix: 1325_bfs

--- beakjoon/silver/1325_bfs.py
+++ beakjoon/silver/1325_bfs.py
@@ -4,21 +4,21 @@ input = sys.stdin.readline
 
 n,m = map(int, input().split())
 A = [[] for _ in range(n+1)]
-
+answer = [0] *(n+1)
 
 def bfs(v):
     queue = deque()
     queue.append(v)
     visited[v] = True
-    cnt = 1
+
     while queue:
         x = queue.popleft()
         for i in A[x]:
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
-                cnt += 1
-    return cnt
+                answer[v] += 1
+
 
 for _ in range(m):
     u,v = map(int, input().split())
@@ -28,12 +28,13 @@ maxValue = 0
 result = []
 for i in range(1,n+1):
     visited = [False] * (n+1)
-    cnts = bfs(i)
-    if maxValue < cnts:
-        maxValue = cnts
+    bfs(i)
+    if maxValue < answer[i]:
+        maxValue = answer[i]
         result.clear()
         result.append(i)
-    elif maxValue == cnts:
+    elif maxValue == answer[i]:
         result.append(i)
 
 print(*result)
+print(*answer)
