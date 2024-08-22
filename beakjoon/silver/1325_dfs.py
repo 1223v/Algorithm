d8b51fcbf0922a524314290a2d89ca7ed2fc8d1f
1223v <1223v@naver.com>
2024-08-22 17:58:55 +0900

    Fix: 1325

--- beakjoon/silver/1325_dfs.py
+++ beakjoon/silver/1325_dfs.py
@@ -2,37 +2,38 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
+n,m = map(int, input().split())
 A = [[] for _ in range(n+1)]
-answer = [0] * (n+1)
 
-def BFS(v):
+
+def bfs(v):
     queue = deque()
     queue.append(v)
     visited[v] = True
+    cnt = 1
     while queue:
         x = queue.popleft()
         for i in A[x]:
             if not visited[i]:
                 visited[i] = True
-                answer[i] += 1
                 queue.append(i)
-
+                cnt += 1
+    return cnt
 
 for _ in range(m):
     u,v = map(int, input().split())
-    A[u].append(v)
+    A[v].append(u)
 
-for i in range(1, n+1):
-    visited = [False] * (n+1)
-    BFS(i)
-
-maxVal = 0
-for i in range(1, n+1):
-    maxVal = max(maxVal, answer[i])
-
+maxValue = 0
+result = []
 for i in range(1,n+1):
-    if maxVal == answer[i]:
-        print(i, end=' ')
+    visited = [False] * (n+1)
+    cnts = bfs(i)
+    if maxValue < cnts:
+        maxValue = cnts
+        result.clear()
+        result.append(i)
+    elif maxValue == cnts:
+        result.append(i)
 
-
+print(*result)
