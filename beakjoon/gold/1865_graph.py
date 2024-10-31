4b5f304497bb50352c8fbfd1ab99c29154da8505
1223v <1223v@naver.com>
2024-11-01 02:19:10 +0900

    Feat: 1865

---
+++ beakjoon/gold/1865_graph.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+
+TC = int(input())
+
+def bf():
+    dist[1] = 0
+    for i in range(N):
+        for j in range(len(edges)):
+            start, end, time_value = edges[j]
+            if dist[end] > dist[start] + time_value:
+                dist[end] = dist[start] + time_value
+                if i == N-1:
+                    return True
+
+    return False
+
+for _ in range(TC):
+    N, M, W = map(int,input().split()) # 지점, 도로 갯수, 웜홀 갯수
+    edges = []
+    dist = [sys.maxsize] * (N+1)
+
+    for _ in range(M): # 양방향
+        S, E, T = map(int,input().split()) # 시작점, 끝점, 걸리는 시간
+        edges.append((S,E,T))
+        edges.append((E, S, T))
+
+
+    for _ in range(W): # 한방향
+        S, E, T = map(int, input().split())  # 시작점, 끝점, 줄어드는 시간
+        edges.append((S, E, -T))
+
+
+
+    if bf():
+        print("YES")
+    else:
+        print("NO")
