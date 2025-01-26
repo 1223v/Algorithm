df4a221057c2cb718b3954a12ad4db971d12e7a5
1223v <1223v@naver.com>
2025-01-26 18:07:21 +0900

    Feat: 1707

---
+++ beakjoon/gold/1707_dfs2.py
@@ -0,0 +1,47 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+TC = int(input())
+
+def dfs(v):
+    global IsEven
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            check[i] = (check[v]+1) % 2
+            dfs(i)
+
+        elif check[v] == check[i]:
+            IsEven = False
+
+for _ in range(TC):
+    N, E = map(int,input().split())
+    IsEven = True
+    graph = [[] for _ in range(N+1)]
+    visited = [0] * (N+1)
+    check = [0] * (N+1)
+
+
+    for _ in range(E):
+        s,e = map(int,input().split())
+        graph[s].append(e)
+        graph[e].append(s)
+
+    for i in range(1,N+1):
+        if IsEven:
+            dfs(i)
+
+        else:
+            break
+
+    if IsEven:
+        print("YES")
+
+    else:
+        print("NO")
+
+
+
+
+
