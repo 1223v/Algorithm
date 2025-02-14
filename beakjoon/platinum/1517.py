f453f8aae13c454b87870031e9d3e76a827de303
1223v <1223v@naver.com>
2025-02-14 21:58:47 +0900

    Feat: 1461,1517

--- beakjoon/platinum/1517.py
+++ beakjoon/platinum/1517.py
@@ -1,50 +1,52 @@
 import sys
-
 input = sys.stdin.readline
+
 result = 0
 
-
-
-
-def merge_sort(s, e):
+def merge_sort(start,end):
     global result
-    if e-s < 1: return
-    m = int(s+(e-s) / 2)
-    merge_sort(s,m)
-    merge_sort(m+1, e)
-    for i in range(s, e+1):
-        tmp[i] = A[i]
 
-    k = s
-    index1 = s
-    index2 = m+1
-    while index1 <=m and index2 <=e:
+    if end - start < 1:
+        return
+
+    mid = int((start + end)/2)
+    merge_sort(start,mid)
+    merge_sort(mid + 1, end)
+
+    for i in range(start, end + 1):
+        tmp[i] = s[i]
+
+    k = start
+    index1 = start
+    index2 = mid + 1
+
+    while index1 <= mid and index2 <= end:
+
         if tmp[index1] > tmp[index2]:
-            A[k] = tmp[index2]
-            result = result + index2 - k #swap 값 카운트
+            s[k] = tmp[index2]
+            result += index2 - k
             k += 1
-            index2 +=1
+            index2 += 1
+
         else:
-            A[k] = tmp[index1]
+            s[k] = tmp[index1]
             k += 1
             index1 += 1
 
-    while index1 <= m:
-        A[k] = tmp[index1]
+    while index1 <= mid:
+        s[k] = tmp[index1]
         k += 1
         index1 += 1
-    while index2 <= e:
-        A[k] = tmp[index2]
+
+    while index2 <= end:
+        s[k] = tmp[index2]
         k += 1
         index2 += 1
 
 
 N = int(input())
-A = list(map(int, input().split()))
-
-A.insert(0,0)
-
-tmp = [0]* int(N+1)
-
+s = list(map(int,input().split()))
+s.insert(0,0)
+tmp = [0] * (N+1)
 merge_sort(1,N)
 print(result)
