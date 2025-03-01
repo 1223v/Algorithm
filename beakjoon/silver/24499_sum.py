570d16e0cbb7e07eb7141fa0d502cc863797859a
1223v <1223v@naver.com>
2025-03-02 02:19:41 +0900

    Feat: 24499_sum

--- beakjoon/silver/24499_sum.py
+++ beakjoon/silver/24499_sum.py
@@ -2,16 +2,20 @@ import sys
 input = sys.stdin.readline
 
 N,K = map(int,input().split())
-k_lst = [0]+list(map(int,input().split()))
-S = [0] * (N+1)
+k_lst = [0] + list(map(int,input().split()))
+k_lst += k_lst[1:-1]
+S = [0] * (N*2)
+S[0] = 0
 S[1] = k_lst[1]
 
-for i in range(1,N):
+
+for i in range(1,(N*2)):
     S[i] = S[i-1] + k_lst[i]
+
 
 max_value = 0
 
-for i in range(1,(N+1)*2):
-    max_value = max(max_value, S[(i+K+1)//K] + S[(i+K+1)%K]-S[(i-1)%K])
+for i in range(N*2-K):
+    max_value = max(max_value, S[i+K] - S[i])
 
 print(max_value)
